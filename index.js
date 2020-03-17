66767ef44e06e1c2e55e6a482fe35dd1265d9fa3
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-03-17 20:16:47 +0900

    java 1 이론

---
+++ index.js
@@ -0,0 +1,79 @@
+/* String
+const what = "Nicolas";
+*/
+
+/* Boolean
+const what = false;
+*/
+/* Number
+const what = 666;
+*/
+/* Float
+const what = 55.1;
+console.log(what);
+*/
+/*
+const monday = "Mon";
+const tue = "Tue";
+const wed = "Wed";
+const thu = "Thu";
+const fri = "Fri";
+
+console.log(monday, tue, wed, thu, fri);
+*/
+/*
+const daysOfWeek = [
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
+  "Sun",
+  "true",
+  true,
+  55
+];
+
+//console.log(daysOfWeek);
+console.log(daysOfWeek[4]);
+*/
+/*
+const nicoInfo = ["Nicolas", "55", true, "Seoul"];
+console.log(nicoInfo);
+*/
+/*
+const nicoInfo = {
+  name: "Nico",
+  age: 33,
+  gender: "Male",
+  isHandsome: true,
+  favMovies: ["Along the gods", "LOTR", "Oldboy"],
+  favFood: [
+    { name: "Kimchi", fatty: false },
+    { name: "Cheese burger", fatty: true }
+  ]
+};
+console.log(nicoInfo.gender);
+nicoInfo.gender = "Female";
+console.log(nicoInfo.gender);
+console.log(nicoInfo.favFood[0].name);
+console.log(console);
+*/
+/*
+console.log("Grettings Nicolas");
+console.log("Hello Jun");
+console.log("Hello Liyn");
+console.log("Hello Dal");
+*/
+/*
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello();
+sayHello("Nicolas");
+*/
+function sayHello(name, age) {
+  console.log("Hello!", name, "you have", age, "years of age.");
+}
+sayHello("Nicolas", "15");
