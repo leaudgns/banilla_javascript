4e3eb270f99ae475581eabacf0db4a91d7835a2d
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-03-17 22:01:04 +0900

    java 연습 2.4

--- index.js
+++ index.js
@@ -124,7 +124,34 @@ console.log(title);
 
 console.error("Fuck");
 */
-const title = document.getElementById("title");
+/*
+const title = document.querySelector("#title");
 title.innerHTML = "Hi! From JS";
 title.style.color = "red";
 document.title = "I own you now";
+*/
+/*
+const title = document.querySelector("#title");
+
+function handleResize() {
+  console.log("I have been resized");
+}
+
+window.addEventListener("resize", handleResize);
+*/
+/*
+const title = document.querySelector("#title");
+
+function handleResize(event) {
+  console.log(event);
+}
+
+window.addEventListener("resize", handleResize);
+*/
+const title = document.querySelector("#title");
+
+function handleClick() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleClick);
